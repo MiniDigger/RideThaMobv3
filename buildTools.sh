b53cb6eecc59b5059d534a9080c4d7de1001ce63
MiniDigger <mini.digger@live.de>
2016-06-13 13:40:20 +0200

    Update buildTools.sh

--- buildTools.sh
+++ buildTools.sh
@@ -1,4 +1,5 @@
 wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+git config --global --unset core.autocrlf
 java -jar BuildTools.jar --rev 1.8.8
 #java -jar BuildTools.jar --rev 1.9
 #java -jar BuildTools.jar --rev 1.9.4
