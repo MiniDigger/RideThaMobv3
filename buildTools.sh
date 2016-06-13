57473f681325580cb5abe553dfc5ad5fb2351b06
MiniDigger <mini.digger@live.de>
2016-06-13 18:34:55 +0200

    Delete buildTools.sh

--- buildTools.sh
+++
@@ -1,8 +0,0 @@
-mkdir bt
-cd bt
-wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-git config --global --unset core.autocrlf
-#java -jar BuildTools.jar --rev 1.8.8
-#java -jar BuildTools.jar --rev 1.9
-#java -jar BuildTools.jar --rev 1.9.4
-java -jar BuildTools.jar --rev 1.10
