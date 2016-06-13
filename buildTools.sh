5ddc9dc70ea1ee16ae80425446998894b6c1e404
MiniDigger <mini.digger@live.de>
2016-06-13 11:58:13 +0200

    Create buildTools.sh

---
+++ buildTools.sh
@@ -0,0 +1,5 @@
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+java -jar BuildTools.jar --rev 1.8.8
+java -jar BuildTools.jar --rev 1.9
+java -jar BuildTools.jar --rev 1.9.4
+java -jar BuildTools.jar --rev 1.10
